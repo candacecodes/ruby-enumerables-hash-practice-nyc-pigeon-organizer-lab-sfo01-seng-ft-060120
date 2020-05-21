c4a4becc9286cbba8c4673f6aa03fb65fa8220ef
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 20:17:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,10 @@
-require pry 
-
 def nyc_pigeon_organizer(data)
-  pigeons = {}
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name| 
+        if !final_array[name]
+          final_array[name] = {}
+        end 
+        if !final_array[name][key]
+          !final_array
 end 
